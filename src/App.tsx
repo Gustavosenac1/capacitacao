7e046dbf4917aa45df72ff7b9de45f300744df0d
Gustavo <gustavomueller94@gmail.com>
2024-05-22 19:56:31 -0300

    greg

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { cn } from "@/lib/utils"
+import { Search } from 'lucide-react'
+
+export default function App() {
+
+  return (
+    <div className="p-2">
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </div>
+  )
+}
