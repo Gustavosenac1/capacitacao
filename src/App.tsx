d52a39d96f6e014d1c04f373107bcb331c705e2a
Gustavo <gustavomueller94@gmail.com>
2024-05-23 11:36:53 -0300

    feat: Add CardWithForm component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { cn } from "@/lib/utils"
-import { NavigationMenuDemo } from "@/itens/menu/menu"
-import Page from "@/itens/menu/page"
+import { cn } from "@/lib/utils";
+import { NavigationMenuDemo } from "@/itens/menu/menu";
+import Page from "@/itens/menu/page";
+import { CardWithForm } from "./itens/menu/card";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <NavigationMenuDemo />
       <div>
         <Page />
+        <CardWithForm />
       </div>
     </div>
   )
